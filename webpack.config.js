1f1c00620af8c71af39eec63b96a0c8dc0ec2598
xadvent <piercestrutt@gmail.com>
2023-10-31 11:55:21 -0600

    Created basic webpack setup for the project.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    devtool: "inline-source-map",
+    output: {
+        filename: 'main.js',
+        clean: true,
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battleship',
+            sourceMap: true,
+            template: './src/index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                // Here is space for more types
+            },
+        ],
+    },
+};
